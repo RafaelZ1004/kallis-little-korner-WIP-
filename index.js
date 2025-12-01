64c29d51efb413301a6b034030b6ff3463423b20
rafael.ang <rafaelang971@gmail.com>
2025-12-01 14:21:57 +0800

    Modified codes in Javascript for the product cards. Made into reusable function instead of repeated code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ links.forEach(link => {
 });
 
 // products list
-const setproducts = [
+const set_products = [
   {
     image:"clothesimages/Sets/S1.jpg",
     choice:"Sets",
@@ -66,7 +66,7 @@ const setproducts = [
     price:"&#8369;100"
   }
 ]
-const pajamasproducts = [
+const pajamas_products = [
   {
     image:"clothesimages/pajamas/P3a.jpg",
     choice:"Pajamas",
@@ -116,7 +116,7 @@ const pajamasproducts = [
     price:"&#8369;100"
   }
 ]
-const dressproducts = [
+const dress_products = [
   {
     image:"clothesimages/Dress/247442372_959400074646001_320232824235129551_n.jpg",
     choice:"Dress",
@@ -166,7 +166,7 @@ const dressproducts = [
     price:"&#8369;100"
   }
 ]
-const paintproducts = [
+const paint_products = [
   {
     image:"images/673f0fff-5c6f-43bd-9743-8f593c3b6ab1.png",
     choice:"Paint by Numbers",
@@ -174,19 +174,19 @@ const paintproducts = [
     price:"&#8369;100"
   },
   {
-    image:"images/sharpened_1.png",
+    image:"images/deer.png",
     choice:"Paint by Numbers",
     name:"Deer",
     price:"&#8369;100"
   },
   {
-    image:"images/sharpened_2.png",
+    image:"images/honeybear.png",
     choice:"Paint by Numbers",
     name:"Bear",
     price:"&#8369;100"
   },
   {
-    image:"images/sharpened_3.png",
+    image:"images/pig.png",
     choice:"Paint by Numbers",
     name:"Pig",
     price:"&#8369;100"
@@ -198,68 +198,26 @@ const pajama = document.getElementById("pajamasproducts");
 const dress = document.getElementById("dressproducts");
 const paint = document.getElementById("paintproducts");
 
+function displayProducts(products_array, sectionID){
+  products_array.forEach(product => {
+  sectionID.innerHTML += `
+    <div class="productcard">
+      <button id="favorite"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#46152F"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg></button>
+      <div class="img-wrapper">
+        <img src="${product.image}" alt="">
+      </div>
+      <button id="add_cart">Add to Cart</button>
+      <div id="belowimg">
+        <p class="productchoice">${product.choice}</p>
+        <h3 class="productname">${product.name}</h3>
+        <p class="productprice">${product.price}</p>
+      </div>
+    </div>
+  `;
+});
+}
 
-setproducts.forEach(product => {
-  set.innerHTML += `
-    <div class="productcard">
-      <button id="favorite"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#46152F"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg></button>
-      <div class="img-wrapper">
-        <img src="${product.image}" alt="">
-      </div>
-      <button id="add_cart">Add to Cart</button>
-      <div id="belowimg">
-        <p class="productchoice">${product.choice}</p>
-        <h3 class="productname">${product.name}</h3>
-        <p class="productprice">${product.price}</p>
-      </div>
-    </div>
-  `;
-});
-pajamasproducts.forEach(product => {
-  pajama.innerHTML += `
-    <div class="productcard">
-      <button id="favorite"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#46152F"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg></button>
-      <div class="img-wrapper">
-        <img src="${product.image}" alt="">
-      </div>
-      <button id="add_cart">Add to Cart</button>
-      <div id="belowimg">
-        <p class="productchoice">${product.choice}</p>
-        <h3 class="productname">${product.name}</h3>
-        <p class="productprice">${product.price}</p>
-      </div>
-    </div>
-  `;
-});
-dressproducts.forEach(product => {
-  dress.innerHTML += `
-    <div class="productcard">
-      <button id="favorite"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#46152F"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg></button>
-      <div class="img-wrapper">
-        <img src="${product.image}" alt="">
-      </div>
-      <button id="add_cart">Add to Cart</button>
-      <div id="belowimg">
-        <p class="productchoice">${product.choice}</p>
-        <h3 class="productname">${product.name}</h3>
-        <p class="productprice">${product.price}</p>
-      </div>
-    </div>
-  `;
-});
-paintproducts.forEach(product => {
-  paint.innerHTML += `
-    <div class="productcard">
-      <button id="favorite"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#46152F"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg></button>
-      <div class="img-wrapper">
-          <img src="${product.image}" alt="">
-      </div>
-      <button id="add_cart">Add to Cart</button>
-      <div id="belowimg">
-        <p class="productchoice">${product.choice}</p>
-        <h3 class="productname">${product.name}</h3>
-        <p class="productprice">${product.price}</p>
-      </div>
-    </div>
-  `;
-});
+displayProducts(set_products, set);
+displayProducts(pajamas_products, pajama);
+displayProducts(dress_products, dress);
+displayProducts(paint_products, paint);
